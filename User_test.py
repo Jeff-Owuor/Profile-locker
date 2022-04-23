8343340d2aca436e0ba56c2a864e801a1bf3f9af
Jeff <Xavierjeff451@gmail.com>
2022-04-23 20:39:06 +0300

    commit

--- User_test.py
+++ User_test.py
@@ -8,4 +8,20 @@ class TestUser(unittest.TestCase):
     '''
     def setUp(self):
         self.new_user = User("SpectreJeff");
+        
+    def test_init(self):
+        '''
+          Test to check if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.user_name,"SpectreJeff")
+        
+    def save_user(self):
+        '''
+          Test to save a user 
+        '''
+        self.save_user_name();
+        self.assertEqual(len(User.users_list),1)
     
+    
+if __name__ == "__main__":
+    unittest.main()
